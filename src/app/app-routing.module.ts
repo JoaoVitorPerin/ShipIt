ed7e8227b6f8421ad492271d64373d73defaf4da
JoaoVitorPerin <joao.vperin@outlook.com>
2023-03-26 13:08:51 -0300

    correção de bugs

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
 
 const routes: Routes = [
-  { path: 'login', component: LoginComponent }
+  { path: 'login', title: 'ShipIt - Login' ,component: LoginComponent },
+  { path: 'home', title: 'ShipIt - Login' ,component: NavBarComponent },
+  {
+    path: '',
+    redirectTo: '/home',
+    pathMatch: 'full'
+  }
 ];
 
 @NgModule({
