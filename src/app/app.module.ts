9186bae04131d1429228fdf9f6e18dba98914977
JoaoVitorPerin <joao.vperin@outlook.com>
2023-04-09 01:56:47 -0300

    Carrousel de imagens criado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,12 +10,14 @@ import {MatButtonModule} from '@angular/material/button';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatIconModule} from '@angular/material/icon';
+import { CarrouselComponent } from './carrousel/carrousel.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    NavBarComponent
+    NavBarComponent,
+    CarrouselComponent
   ],
   imports: [
     BrowserModule,
