6e4516d2c9402bf9881a05ae40431e91f042168a
JoaoVitorPerin <joao.vperin@outlook.com>
2023-05-22 20:52:26 -0300

    Adicionando cards na home dinamicamente

--- ShipIt-front/src/app/cards/cards.component.ts
+++ ShipIt-front/src/app/cards/cards.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { CadastroItemModel } from '../cadastro-item/model/cadastro_item.model';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-cards',
@@ -6,5 +10,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./cards.component.css']
 })
 export class CardsComponent {
+  public listProdutos: Array<CadastroItemModel> = [];
 
+  constructor(private httpClient: HttpClient) { }
+
+  ngOnInit(){
+    this.listar();
+  }
+
+  listar() {
+    this.httpClient.get<CadastroItemModel[]>("http://localhost:8080/produto")
+      .subscribe(
+        (produtos: CadastroItemModel[]) => {
+          this.listProdutos = produtos;
+          console.log(this.listProdutos);
+        },
+        (error) => {
+          console.error(error);
+        }
+      );
+  }
 }
