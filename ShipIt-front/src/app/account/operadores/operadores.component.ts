c0912077bda59770c824e866d6ef0597b8bc485b
Gabriel Lima <80799343+OGabrielLima@users.noreply.github.com>
2023-06-11 18:26:53 -0300

    Controle de Usuarios administrativo

---
+++ ShipIt-front/src/app/account/operadores/operadores.component.ts
@@ -0,0 +1,38 @@
+import { Component, OnInit } from '@angular/core';
+import { UsuarioModel } from '../model/usuario.model';
+import { AccountService } from '../shared/account.service';
+@Component({
+  selector: 'app-operadores',
+  templateUrl: './operadores.component.html',
+  styleUrls: ['./operadores.Component.scss']
+})
+export class OperadoresComponent implements OnInit {
+
+  idUsuario: number | undefined;
+  nomeUsuario: string = '';
+  emailUsuario: string = '';
+
+
+  displayedColumns: string[] = ['idUsuario', 'nomeUsuario', 'emailUsuario', 'delete'];
+  usuarios : UsuarioModel[] = [];
+
+  constructor(private usuarioService : AccountService) { }
+
+  ngOnInit(): void {
+    this.listar()
+  }
+
+  listar(){
+    this.usuarioService.listar().subscribe(usuarios => {
+      console.log(usuarios);
+      this.usuarios = usuarios;
+    })
+  }
+
+  deletar(idUsuario : number){
+    this.usuarioService.deletar(idUsuario).subscribe(usuario=>{
+      this.listar();
+    })
+  }
+
+}
